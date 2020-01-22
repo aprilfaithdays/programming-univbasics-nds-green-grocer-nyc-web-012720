4f897a761520e073743a8bffc2cd5fbd50fbcb6c
April Escobar <aprilfaith.e@gmail.com>
2020-01-22 01:20:41 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -55,7 +55,7 @@ def apply_coupons(cart, coupons)
  while i < coupons.length do
    
    cart_item = find_item_by_name_in_collection(coupons[i][:item], cart)
-   couponed_item_name = "#{coupons[i][:item]} W/ COUPON"
+   couponed_item_name = "#{coupons[i][:item]} W/COUPON"
    cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name, cart)
   
   if cart_item && cart_item[:count] >= coupons[i][:num]
